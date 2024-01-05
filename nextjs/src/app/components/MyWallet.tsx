c4dabfb3fa12396cf46e86a37238c104cb18e213
viktorhugodev <viktorhugo.dev@gmail.com>
2024-01-05 09:20:40 -0300

    add: flow-bite and table component

--- nextjs/src/app/components/MyWallet.tsx
+++ nextjs/src/app/components/MyWallet.tsx
@@ -1,4 +1,13 @@
 import { WalletAsset } from '@/models'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+} from './flowbite-components'
+import Link from 'next/link'
 
 interface MyWallet {
   wallet_id: string
@@ -17,12 +26,34 @@ async function getWalletAssets(wallet_id: string): Promise<WalletAsset[]> {
 export async function MyWallet({ wallet_id }: MyWallet) {
   const walletAssets = await getWalletAssets(wallet_id)
   return (
-    <ul>
-      {walletAssets.map(walletAsset => (
-        <li key={walletAsset.id}>
-          {walletAsset.Asset.id} - {walletAsset.shares} - R$ {walletAsset.Asset.price}
-        </li>
-      ))}
-    </ul>
+    <Table>
+      <TableHead>
+        <TableHeadCell>Nome</TableHeadCell>
+        <TableHeadCell>Preço R$</TableHeadCell>
+        <TableHeadCell>Quant.</TableHeadCell>
+        <TableHeadCell>
+          <span className='sr-only'>Comprar/Vender</span>
+        </TableHeadCell>
+      </TableHead>
+      <TableBody className='divide-y'>
+        {walletAssets!.map((walletAsset, key) => (
+          <TableRow className='border-gray-700 bg-gray-800' key={key}>
+            <TableCell className='whitespace-nowrap font-medium text-white'>
+              {walletAsset.Asset.id} ({walletAsset.Asset.symbol})
+            </TableCell>
+            <TableCell>{walletAsset.Asset.price}</TableCell>
+            <TableCell>{walletAsset.shares}</TableCell>
+            <TableCell>
+              <Link
+                className='font-medium hover:underline text-cyan-500'
+                href={`/${wallet_id}/${walletAsset.Asset.id}`}
+              >
+                Comprar/Vender
+              </Link>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/flowbite-react/lib/**/*.js',
+    './node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {
