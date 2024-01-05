415ae03e4a8605b5855bf88977192872d4b7fc7b
viktorhugodev <viktorhugo.dev@gmail.com>
2024-01-05 12:59:58 -0300

    add: navbar

--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -16,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require('flowbite/plugin'), require('flowbite-typography')],
 }
 export default config
