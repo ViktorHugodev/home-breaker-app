d98a49f720fb09b65b4c68bf9eafd7b90800fecb
viktorhugodev <viktorhugo.dev@gmail.com>
2024-02-06 10:21:26 -0300

    feat: assetchart update

--- nextjs/src/app/components/AssetChartComponent.tsx
+++ nextjs/src/app/components/AssetChartComponent.tsx
@@ -1,15 +1,50 @@
 'use client'
 
+import { AssetDaily } from '@/models'
 import { fetcher } from '@/utils'
-import { ChartComponent } from './CharComponent'
-import useSWR from 'swr'
+import { MutableRefObject, useRef } from 'react'
 
-interface AssetChartProps {
-  asset_id: string
-}
-export const AssetChartComponent = ({ asset_id }: AssetChartProps) => {
-  const { data: asset, mutate } = useSWR(`http://localhost:3000/assets/${asset_id}`, fetcher, {
-    fallbackData: { id: asset_id, price: 0 },
-  })
-  return <ChartComponent header={`${asset_id} - R$ ${asset?.price}`} />
+import useSWR from 'swr'
+import useSWRSubscription, { SWRSubscriptionOptions } from 'swr/subscription'
+import { ChartComponent, ChartComponentRef } from './CharComponent'
+
+export const AssetChartComponent = (props: { asset_id: string }) => {
+  const chartRef = useRef() as MutableRefObject<ChartComponentRef>
+
+  const { data: asset, mutate } = useSWR(
+    `http://localhost:3000/assets/${props.asset_id}`,
+    fetcher,
+    {
+      fallbackData: { id: props.asset_id, price: 0 },
+    },
+  )
+
+  const { data: assetDaily } = useSWRSubscription(
+    `http://localhost:3000/assets/${props.asset_id}/daily/events`,
+    (path, { next }: SWRSubscriptionOptions) => {
+      const eventSource = new EventSource(path)
+      eventSource.addEventListener('asset-daily-created', async event => {
+        console.log(event)
+        const assetDailyCreated: AssetDaily = JSON.parse(event.data)
+        chartRef.current.update({
+          time: new Date(assetDailyCreated.date).getTime(),
+          value: assetDailyCreated.price,
+        })
+        await mutate({ id: assetDailyCreated.id, price: assetDailyCreated.price }, false)
+        next(null, assetDailyCreated)
+      })
+
+      eventSource.onerror = event => {
+        console.log('error - assetChart', event)
+        eventSource.close()
+      }
+      return () => {
+        console.log('close event source')
+        eventSource.close()
+      }
+    },
+    {},
+  )
+
+  return <ChartComponent header={`${props.asset_id} - R$ ${asset.price}`} ref={chartRef} />
 }
